515fa74770f0ffe82db5b2590444c82308eeba1e
alapuly <152487735+alapuly@users.noreply.github.com>
2024-01-03 19:01:50 +0100

    Create interactive burger menu (responsive)

---
+++ js/CJ_Pizzeria.js
@@ -0,0 +1,5 @@
+function uncover() {
+    document.querySelector('.burger-links').style.display =
+        (document.querySelector('.burger-links').style.display == 'block') ? 'none' : 'block';
+        //
+}
